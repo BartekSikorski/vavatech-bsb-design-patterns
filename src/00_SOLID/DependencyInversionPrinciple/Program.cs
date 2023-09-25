9c6939450620e7681a095341a26cad96a1575b9d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-25 11:58:21 +0200

    Refaktoring

--- src/00_SOLID/DependencyInversionPrinciple/Program.cs
+++ src/00_SOLID/DependencyInversionPrinciple/Program.cs
@@ -2,16 +2,15 @@
 // Wszystkie zależności powinny w jak największym stopniu zależeć od abstrakcji a nie od konkretnego typu.
 // Oznacza to, że w kodzie powinny być używane interfejsy lub klasy abstrakcyjne, zamiast bezpośrednio operować na konkretnych klasach.
     
-// Przykład łamiący zasadę odwracania zależności
 
 var controller = new ProductController(new DbProductRepository());
 var product = controller.Get(1);
 
 public class ProductController
 {
-    private DbProductRepository repository;
+    private IProductRepository repository;
 
-    public ProductController(DbProductRepository repository)
+    public ProductController(IProductRepository repository)
     {
         this.repository = repository;
     } 
@@ -27,7 +26,13 @@ public class ProductController
     }
 }
 
-public class DbProductRepository
+public interface IProductRepository
+{
+    Product GetById(int id);
+    void Add(Product product);
+}
+
+public class DbProductRepository : IProductRepository
 {
     public Product GetById(int id)
     {
@@ -40,8 +45,18 @@ public class DbProductRepository
     }
 }
 
-public class CloudProductRepository
+public class CloudProductRepository : IProductRepository
 {
+    public void Add(Product product)
+    {
+        Save(product);
+    }
+
+    public Product GetById(int id)
+    {
+        return Load(id);
+    }
+
     public Product Load(int id)
     {
         throw new NotImplementedException();
