73e68502207ed007fdd0a73c6dff6d77c8168932
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-27 11:21:15 +0200

    Dodanie strategii

---
+++ src/03_BehavioralsPatterns/StrategyPattern/GenderFixedDiscountCalculatorStrategy.cs
@@ -0,0 +1,19 @@
+﻿namespace StrategyPattern
+{
+    public class GenderFixedDiscountCalculatorStrategy : IDiscountCalculatorStrategy
+    {
+        private readonly Gender gender;
+
+        private readonly decimal amount;
+
+        public GenderFixedDiscountCalculatorStrategy(Gender gender, decimal amount)
+        {
+            this.gender = gender;
+            this.amount = amount;
+        }
+
+        public decimal NoDiscount => decimal.Zero;
+        public bool CanDiscount(Order order) => order.Customer.Gender == gender;
+        public decimal Discount(Order order) => amount;
+    }
+}
---
+++ src/03_BehavioralsPatterns/StrategyPattern/HappyHoursFixedDiscountCalculatorStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    // Concrete Strategy A
+    // Happy Hours - upust kwotowy w godzinach od 9 do 15
+    public class HappyHoursFixedDiscountCalculatorStrategy : IDiscountCalculatorStrategy
+    {
+        private readonly TimeSpan from;
+        private readonly TimeSpan to;
+
+        private readonly decimal amount;
+
+        public HappyHoursFixedDiscountCalculatorStrategy(TimeSpan from, TimeSpan to, decimal amount)
+        {
+            this.from = from;
+            this.to = to;
+            this.amount = amount;
+        }
+
+        public decimal NoDiscount => decimal.Zero;
+
+        public bool CanDiscount(Order order)
+        {
+            return order.OrderDate.TimeOfDay >= from && order.OrderDate.TimeOfDay <= to;
+        }
+
+        public decimal Discount(Order order)
+        {
+            return amount;
+        }
+    }
+}
