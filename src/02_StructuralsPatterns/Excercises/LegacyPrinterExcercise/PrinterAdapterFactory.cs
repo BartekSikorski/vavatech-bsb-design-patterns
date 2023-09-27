b59e7529bfcdb97bed0ff8bc6ec787c9d7b07c53
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-27 09:47:33 +0200

    Refaktoring

---
+++ src/02_StructuralsPatterns/Excercises/LegacyPrinterExcercise/PrinterAdapterFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyPrinterExcercise
+{
+ 
+    // Adapter
+    public class PrinterTypeMapper
+    {
+        public static PrinterType Map(bool legacy)
+        {
+            return legacy ? PrinterType.Legacy : PrinterType.Modern;
+        }
+    }
+
+    public class PrinterAdapterFactory
+    {                
+        public static IPrinterAdapter Create(PrinterType printerType) 
+        {
+            switch(printerType)
+            {
+                case PrinterType.Legacy: return new LegacyPrinterAdapter();
+                case PrinterType.Modern: return new Printer();
+
+                default: throw new NotImplementedException();
+            }
+        
+        }
+    }
+}
--- src/02_StructuralsPatterns/Excercises/LegacyPrinterExcercise/Program.cs
+++ src/02_StructuralsPatterns/Excercises/LegacyPrinterExcercise/Program.cs
@@ -1,40 +1,62 @@
 ﻿
 // TODO: Dodaj obliczanie kosztu wydruku do starej drukarki
 
-bool legacy = false;
+using LegacyPrinterExcercise;
+using System.Reflection;
+
+bool legacy = true;
 
 string document = "Hello World!";
 int copies = 3;
 
+IPrinterAdapter printer = PrinterAdapterFactory.Create(PrinterTypeMapper.Map(legacy));
 
-if (legacy)
+printer = new CounterPrinterProxy(new PrinterProxy(printer));
+
+printer.Print(document, copies);
+
+printer.Print(document, copies);
+printer.Print(document, copies);
+
+
+
+
+
+public class CounterPrinterProxy : IPrinterAdapter
 {
-    LegacyPrinter printer = new LegacyPrinter();
+    private readonly IPrinterAdapter printer;
 
-    for (int i = 0; i < copies; i++)
+    public int Counter { get; private set; }
+
+    public CounterPrinterProxy(IPrinterAdapter printer)
     {
-        printer.PrintDocument(document);
+        this.printer = printer;
     }
-}
-else
-{
-    Printer printer = new Printer();
-    printer.Print(document, copies);
-}
-
-
-
-
-public class Printer
-{
-    decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
 
     public void Print(string document, int copies = 1)
     {
-        for (int copy = 1; copy <= copies; copy++)
-        {
-            Console.WriteLine($"Printer is printing: {document}");
-        }
+        printer.Print(document, copies);
+
+        Counter += copies;
+    }
+}
+
+public class PrinterProxy : IPrinterAdapter
+{
+    decimal _costPerCopy; // Cost of 0.10 zł per copy
+
+    // Real subject
+    private readonly IPrinterAdapter printer;
+
+    public PrinterProxy(IPrinterAdapter printerAdapter, decimal costPerCopy = 0.1m)
+    {
+        this.printer = printerAdapter;
+        _costPerCopy = costPerCopy;
+    }
+
+    public void Print(string document, int copies = 1)
+    {
+        printer.Print(document, copies);
 
         var cost = CalculateCost(copies);
         Console.WriteLine($"{cost:C2}");
@@ -48,11 +70,53 @@ public class Printer
 }
 
 
+public interface IPrinterAdapter
+{
+    void Print(string document, int copies = 1);
+}
+
+public class Printer : IPrinterAdapter
+{
+    public void Print(string document, int copies = 1)
+    {
+        for (int copy = 1; copy <= copies; copy++)
+        {
+            Console.WriteLine($"Printer is printing: {document}");
+        }
+    }
+}
+
+
 // Nie zmieniaj kodu tej starej klasy!
+
+public class LegacyPrinterAdapter : IPrinterAdapter
+{
+    private readonly LegacyPrinter printer;
+
+    public LegacyPrinterAdapter()
+    {
+        printer = new LegacyPrinter();
+    }
+    public void Print(string document, int copies = 1)
+    {
+        for (int i = 0; i < copies; i++)
+        {
+            printer.PrintDocument(document);
+        }
+    }
+}
+
 public class LegacyPrinter
 {
     public void PrintDocument(string document)
     {
         Console.WriteLine($"Legacy Printer is printing: {document}");
     }
+}
+
+public enum PrinterType
+{
+    Legacy,
+    Modern,
+    Color
 }
