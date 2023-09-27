036e4dcf3fc51d4eee79e80cc3068348285f8dc1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-27 11:00:46 +0200

    Refaktoring

---
+++ src/03_BehavioralsPatterns/TemplateMethodPattern/GenderFixedOrderCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace TemplateMethodPattern
+{
+    public class GenderFixedOrderCalculator
+    {
+        private readonly Gender gender;
+
+        private readonly decimal amount;
+
+        public GenderFixedOrderCalculator(Gender gender, decimal amount)
+        {
+            this.gender = gender;
+            this.amount = amount;
+        }
+
+        public decimal CalculateDiscount(Order order)
+        {
+            if (order.Customer.Gender == gender)              // Warunek (Predykat)
+            {
+                return amount;             // Upust (Discount)
+            }
+            else
+                return 0;                                     // Brak upustu (NoDiscount)
+        }
+    }
+}
