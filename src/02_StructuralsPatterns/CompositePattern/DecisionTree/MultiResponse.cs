22c5adf796d2a9ee54c3f161b65dc231acf8d209
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-09-27 19:12:54 +0200

    Przeniesienie do osobnych plików

---
+++ src/02_StructuralsPatterns/CompositePattern/DecisionTree/MultiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace CompositePattern.DecisionTree
+{
+    public class MultiResponse
+    {
+        private Dictionary<ConsoleKey, Node> nodes = new Dictionary<ConsoleKey, Node>();
+        public void AddResponse(ConsoleKey key, Node value)
+        {
+            nodes.Add(key, value);
+        }
+        
+        public Node Response => nodes[Console.ReadKey().Key];
+    }
+
+
+}
--- src/02_StructuralsPatterns/CompositePattern/DecisionTree/Question.cs
+++ src/02_StructuralsPatterns/CompositePattern/DecisionTree/Question.cs
@@ -1,17 +1,6 @@
 ﻿namespace CompositePattern.DecisionTree
 {
 
-    public class MultiResponse
-    {
-        private Dictionary<ConsoleKey, Node> nodes = new Dictionary<ConsoleKey, Node>();
-        public void AddResponse(ConsoleKey key, Node value)
-        {
-            nodes.Add(key, value);
-        }
-        
-        public Node Response => nodes[Console.ReadKey().Key];
-    }
-
     // Concrete Component A
     public class Question : Node
     {
